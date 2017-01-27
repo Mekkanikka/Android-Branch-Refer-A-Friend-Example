0b771e96c616f1ff272cda00ce6fc87e88ea8d3d
EvangelosG <egroth@gmail.com>
2017-01-27 14:59:21 -0800

    new lines? why not one less.

--- app/src/main/java/io/branch/demoapp/MainActivity.java
+++ app/src/main/java/io/branch/demoapp/MainActivity.java
@@ -97,7 +97,6 @@ public class MainActivity extends AppCompatActivity {
                                 startActivity(i);
                             }
                         } catch (JSONException e) {
-
                         }
                     } else {
                         //do stuff without Branch link data
